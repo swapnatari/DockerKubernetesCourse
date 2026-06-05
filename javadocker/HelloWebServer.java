115489d86a86a2c4fada53bcbb44415aed242862
swapnatari <swapnatarigonda@gmail.com>
2026-06-05 11:07:47 +0530

    git commit -m "save"

--- javadocker/HelloWebServer.java
+++ javadocker/HelloWebServer.java
@@ -19,7 +19,7 @@ public class HelloWebServer {
         @Override
         public void handle(HttpExchange exchange) throws IOException {
             String response = "<html><head><title>Java Docker Web</title></head>"
-                    + "<body><h1>Hello from Java in Docker!</h1>"
+                    + "<body><h1>Hello from Java in Docker! Swapna</h1>"
                     + "<p>This is a simple web-based Java app running inside a container.</p>"
                     + "</body></html>";
             exchange.getResponseHeaders().set("Content-Type", "text/html; charset=UTF-8");
